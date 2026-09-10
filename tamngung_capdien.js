9294a4689019bb1bd545cdfa1eeaa6ef1a19dc73
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-10 20:26:53 +0700

    Update tamngung_capdien.js

--- tamngung_capdien.js
+++ tamngung_capdien.js
@@ -96,7 +96,7 @@ function updateStatsSummary() {
   const statsEl = document.getElementById('statsSummary');
   if (statsEl) {
     const uncutCount = getUncutCount();
-    statsEl.innerHTML = `Tổng khách hàng: ${allCustomers.length} - Chưa thực hiện: <span style=color:red;>${uncutCount}</span>`;
+    statsEl.innerHTML = `Tổng khách hàng: <span style=color:blue;>${allCustomers.length}</span> - Chưa thực hiện: <span style=color:red;>${uncutCount}</span>`;
   }
 }
 
