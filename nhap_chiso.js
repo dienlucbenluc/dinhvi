d3f84cab0b860c50bd253fcd395edf0e8cb341a6
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-22 21:27:09 +0700

    Update nhap_chiso.js

--- nhap_chiso.js
+++ nhap_chiso.js
@@ -1,8 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwjw5x47mNLBpC3Ar4beIIM20XzZJAVXMLusNZV2rHbyCvls7pICldt7UAkM6htgqpa/exec";
-
-// Cấu hình Cloudinary
-const CLOUDINARY_UPLOAD_PRESET = "image_chiso"; // Thay bằng upload_preset của bạn nếu cần
-const CLOUDINARY_CLOUD_NAME = "jokzcdxt";       // Thay bằng cloud_name của bạn nếu cần
+const CLOUDINARY_CLOUD_NAME = "jokzcdxt";  
+const CLOUDINARY_UPLOAD_PRESET = "image_chiso";
 const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
 
 let currentUser = null;
@@ -121,11 +119,10 @@ async function uploadToCloudinary(file, maKhang = "") {
   // Đặt public_id để Cloudinary lưu đúng tên file mà không tự sinh chuỗi ngẫu nhiên
   formData.append("public_id", customFileName);
   
-  const res = await fetch(CLOUDINARY_UPLOAD_URL, {
-    method: "POST",
-    body: formData
-  });
-
+  const res = await fetch(
+    `https://api.cloudinary.com/v1_1/${encodeURIComponent(CLOUDINARY_CLOUD_NAME)}/image/upload`,
+  {method: "POST",body: formData}
+   );
   const data = await res.json();
   if (data.secure_url) {
     return data.secure_url;
