da5ce4ac0ffc001bd673ad7bb9a2da123c75e543
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-22 15:08:31 +0700

    Update nhap_chiso.js

--- nhap_chiso.js
+++ nhap_chiso.js
@@ -692,7 +692,7 @@ async function deleteImageFromCloudinary(publicIdOrUrl) {
       headers: { "Content-Type": "text/plain;charset=utf-8" },
       body: JSON.stringify({
         action: "DELETE_CLOUDINARY_IMAGE",
-        public_id:  data.public_id,
+        public_id:  public_id,
         cloud_name: CLOUDINARY_CLOUD_NAME
       })
     });
