c40cebfd4649ac299466f9a6b71a2642b6da21b8
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-22 14:28:18 +0700

    Update nhap_chiso.js

--- nhap_chiso.js
+++ nhap_chiso.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyKaG42B8RFzHToMu2Gqk7y5mCQ4wqDxxB5NWftA5lOZdB_mrLkCy6GkVs7zyOgRrHd/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwjw5x47mNLBpC3Ar4beIIM20XzZJAVXMLusNZV2rHbyCvls7pICldt7UAkM6htgqpa/exec";
 
 // Cấu hình Cloudinary
 const CLOUDINARY_CLOUD_NAME = 'jokzcdxt';
