1d8fdd5a4be72eb02a0d323e64a0518ee816361c
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-23 07:09:54 +0700

    Update nhap_chiso.js

--- nhap_chiso.js
+++ nhap_chiso.js
@@ -1,6 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwjw5x47mNLBpC3Ar4beIIM20XzZJAVXMLusNZV2rHbyCvls7pICldt7UAkM6htgqpa/exec";
 const CLOUDINARY_CLOUD_NAME = "jokzcdxt";  
-const CLOUDINARY_UPLOAD_PRESET = "image_chiso";
+const CLOUDINARY_UPLOAD_PRESET = "image_catdien";
 
 let currentUser = null;
 let groupedData = {};
