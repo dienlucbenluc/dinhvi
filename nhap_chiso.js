9f3f72b60a1b865c5689911f2dc512dbcb71b61d
dienlucbenluc <truyennd.dlbl@gmail.com>
2026-09-24 16:32:09 +0700

    Update nhap_chiso.js

--- nhap_chiso.js
+++ nhap_chiso.js
@@ -107,7 +107,7 @@ async function checkAndLoadInitialData() {
     return;
   }
 
-  showToast("⏳ Đang lấy dữ liệu chỉ số v1...");
+  showToast("⏳ Đang lấy dữ liệu chỉ số...");
   try {
     const res = await fetch(API_URL, {
       method: "POST",
